f49c239524c882bf116a5ea81ac54400094d19bc
Helen Silva <helen_silva404@hotmail.com>
2026-06-04 13:52:04 -0300

    chore: configs inicial

---
+++ src/main/java/com/cadastro/pessoas/PessoasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cadastro.pessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cadastro/pessoas/PessoasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadastro.pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
