5219dec24d98a24204df0b374d77ad561cbbedf3
Helen Silva <helen_silva404@hotmail.com>
2026-06-04 14:43:23 -0300

    feat: add validador de data de nascimento

--- src/main/java/com/cadastro/pessoas/exception/DataNascimentoValida.java
+++ src/main/java/com/cadastro/pessoas/exception/DataNascimentoValida.java
@@ -1,4 +1,19 @@
 package com.cadastro.pessoas.exception;
 
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = DataNascimentoValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface DataNascimentoValida {
+
+    String message() default "Data de nascimento inválida";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
