f683f4303b8232746f3cae9a531f09b4b2932cd6
Helen Silva <helen_silva404@hotmail.com>
2026-06-04 14:51:38 -0300

    ci: add github actions + ajusta teste padrão

--- src/test/java/com/cadastro/pessoas/PessoasBackendApplicationTests.java
+++ src/test/java/com/cadastro/pessoas/PessoasBackendApplicationTests.java
@@ -1,13 +1,11 @@
 package com.cadastro.pessoas;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PessoasBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// testes unitários serão adicionados dps
 	}
-
 }
