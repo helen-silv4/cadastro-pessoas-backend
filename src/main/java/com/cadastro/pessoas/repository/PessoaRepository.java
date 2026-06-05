8eba3ecba9830e7acf0f53d5745c2699d828e357
Helen Silva <helen_silva404@hotmail.com>
2026-06-04 22:12:56 -0300

    feat: adiciona PessoaRepository

--- src/main/java/com/cadastro/pessoas/repository/PessoaRepository.java
+++ src/main/java/com/cadastro/pessoas/repository/PessoaRepository.java
@@ -1,4 +1,18 @@
 package com.cadastro.pessoas.repository;
 
-public class PessoaRepository {
+import com.cadastro.pessoas.entity.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.Set;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+    boolean existsByDocumento(String documento);
+
+    boolean existsByEmail(String email);
+
+    @Query("SELECT p.login FROM Pessoa p")
+    Set<String> findAllLogins();
 }
